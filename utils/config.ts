3007129be8c9d5152690f7d0672690a441e1d858
Tushar Shukla <tushareshukla@gmail.com>
2025-07-31 23:13:08 +0530

    update config URLs for production environment

--- utils/config.ts
+++ utils/config.ts
@@ -1,7 +1,7 @@
 // vcard stage
 export const config = {
-    BACKEND_URL: "https://sandbox-api.delightloop.ai",
-    FRONTEND_URL: "https://vcard-delight.patchup.health/"
+    BACKEND_URL: "https://app.delighto.ai",
+    FRONTEND_URL: "https://delighto.ai/",
 }
 // local
 // export const config = {
