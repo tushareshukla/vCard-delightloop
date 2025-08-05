c8dbf3c2752b009501fd788c217e1d9cf201bf89
milan <milanparmar.me@gmail.com>
2025-08-05 19:02:48 +0530

    update

--- utils/config.ts
+++ utils/config.ts
@@ -1,15 +1,15 @@
 // delighto vcard stage
-// export const config = {
-//     BACKEND_URL: "https://api.delighto.ai",
-//     FRONTEND_URL: "https://delighto.ai/",
-// }
+export const config = {
+    BACKEND_URL: "https://api.delighto.ai",
+    FRONTEND_URL: "https://delighto.ai/",
+}
 // local
 // export const config = {
 //   BACKEND_URL: "https://api.delightloop.ai",
 //   FRONTEND_URL: "http://localhost:3000/",
 // };
 // sandbox
-export const config = {
-  BACKEND_URL: "https://sandbox-api.delightloop.ai",
-  FRONTEND_URL: "https://vcard-delight.patchup.health/",
-};
+// export const config = {
+//   BACKEND_URL: "https://sandbox-api.delightloop.ai",
+//   FRONTEND_URL: "https://vcard-delight.patchup.health/",
+// };
